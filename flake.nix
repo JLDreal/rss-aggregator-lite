795cc9011522a3ab7185792ef4fd3e9c513d05e1
Y <you@example.com>
2025-04-29 18:06:53 +0200

    trying to install diesel

--- flake.nix
+++ flake.nix
@@ -23,13 +23,15 @@
             clang
             llvmPackages.bintools
             rustup
+            pkg-config
+            sqlite
           ];
 
           RUSTC_VERSION = overrides.toolchain.channel;
-          
+
           # https://github.com/rust-lang/rust-bindgen#environment-variables
           LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-          
+
           shellHook = ''
             export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
             export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
@@ -39,10 +41,10 @@
           RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
             # add libraries here (e.g. pkgs.libvmi)
           ]);
-          
+
           LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (buildInputs ++ nativeBuildInputs);
 
-          
+
           # Add glibc, clang, glib, and other headers to bindgen search path
           BINDGEN_EXTRA_CLANG_ARGS =
           # Includes normal include path
